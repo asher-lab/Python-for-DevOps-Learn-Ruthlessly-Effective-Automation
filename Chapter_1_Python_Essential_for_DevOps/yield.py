b21a71a33a3849b994d6d2922a4072fb6a3725b5
Asher <51806917+asher-lab@users.noreply.github.com>
2021-04-23 14:45:41 +0800

    Create yield.py

---
+++ Chapter_1_Python_Essential_for_DevOps/yield.py
@@ -0,0 +1,13 @@
+# A Simple Python program to demonstrate working
+# of yield
+
+# A generator function that yields 1 for the first time,
+# 2 second time and 3 third time
+def simpleGeneratorFun():
+	yield 1
+	yield 2
+	yield 3
+
+# Driver code to check above generator function
+for value in simpleGeneratorFun():
+	print(value)
