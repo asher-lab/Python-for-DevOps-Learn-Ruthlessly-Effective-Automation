8b28ca6ab8caeca69cfff895a5c704edc0416929
Asher <51806917+asher-lab@users.noreply.github.com>
2021-04-23 12:32:38 +0800

    Create generator_compre.py

---
+++ Chapter_1_Python_Essential_for_DevOps/generator_compre.py
@@ -0,0 +1,17 @@
+list_o_nums = [x for x in range(100)]
+gen_o_nums = (x for x in range (100))
+
+print(list_o_nums)
+print(gen_o_nums)
+
+#import sys module
+import sys
+
+#get the size of the variab;es
+sys.getsizeof(list_o_nums)
+sys.getsizeof(gen_o_nums)
+
+#Iterating the value of the generator
+print()
+for i in range(100):
+  print(next(gen_o_nums))
