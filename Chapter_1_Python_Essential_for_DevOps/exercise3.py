d874a01416dfd4392218f102a2043d2ad7c99cf1
Asher <51806917+asher-lab@users.noreply.github.com>
2021-04-23 14:40:45 +0800

    Create exercise3.py

---
+++ Chapter_1_Python_Essential_for_DevOps/exercise3.py
@@ -0,0 +1,8 @@
+# Iterating Using Loops
+letters = []
+for letter in 'smogtether':
+  letters.append(letter)
+print(letters)
+converted_list = [x.upper() for x in letters]
+print(converted_list)
+  
