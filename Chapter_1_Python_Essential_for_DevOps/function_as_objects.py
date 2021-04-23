70f67fd20e89aae5cb3a00980a85e456f47f94e8
Asher <51806917+asher-lab@users.noreply.github.com>
2021-04-23 09:10:29 +0800

    Update function_as_objects.py

--- Chapter_1_Python_Essential_for_DevOps/function_as_objects.py
+++ Chapter_1_Python_Essential_for_DevOps/function_as_objects.py
@@ -1,6 +1,8 @@
 def double(input):
   '''double input'''
   print(input * 2)
+  print("")
+  return input*2
   
   
 #call the functio
@@ -12,7 +14,9 @@ type(double)
 def triple(input):
   '''triple input'''
   print(input * 3)
-  
+  print("")
+  return input * 3
+
 #Set the function to double and triple for later iteration
 functions = [double, triple]
 
