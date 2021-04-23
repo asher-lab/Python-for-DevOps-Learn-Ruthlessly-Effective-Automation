e6838e1580b9aa99f06d7d764eca03d94cbfd56f
Asher <51806917+asher-lab@users.noreply.github.com>
2021-04-23 13:51:18 +0800

    Create exercise2.py

---
+++ Chapter_1_Python_Essential_for_DevOps/exercise2.py
@@ -0,0 +1,12 @@
+# Function to determine the inputted strng as either Uppercase or LowerCase , isUpper = true if all char in the string are uppercase, false if neither of one re 
+
+def check(input):
+  if(isUpper(input)):
+   print("String has all char cases in UpperCase")
+  else:
+    print("String lowercase")
+    
+string = input("Input string: ")
+check(string)
+
+#What if it is an number? That is an error in a production setting.
