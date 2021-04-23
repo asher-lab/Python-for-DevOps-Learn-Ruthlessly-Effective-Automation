b3982a0f67e2ec1867acc319f3ce397edbae0171
Asher <51806917+asher-lab@users.noreply.github.com>
2021-04-24 07:01:15 +0800

    Update readingfiles.py
    text.txt """A text can be any example
     of written or spoken languag
    e, from something as complex as a
     book or legal document to some
    thing as simple as the body
     of an email or the words on the b
    ack of a cereal box. In the humanities, different fields of study concern themselves with different forms of texts.
    """

--- Chapter_2_Automating_Files_and_the_File_system/readingfiles.py
+++ Chapter_2_Automating_Files_and_the_File_system/readingfiles.py
@@ -5,4 +5,11 @@ print(len(text))
 
 print(text[56])
 print(open_file)
-open_file.close
+open_file.close()
+
+#Using readlines
+open_file = open(file_path, 'r')
+text = open_file.readlines()
+print(len(text))
+print(text[56])
+open_file.close()
