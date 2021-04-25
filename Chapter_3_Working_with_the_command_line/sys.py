f1af0c2779d29f16c8b9dbd9dc2929b15806d3a7
Asher <51806917+asher-lab@users.noreply.github.com>
2021-04-25 13:30:47 +0800

    Update sys.py

--- Chapter_3_Working_with_the_command_line/sys.py
+++ Chapter_3_Working_with_the_command_line/sys.py
@@ -1,1 +1,18 @@
+import sys
 
+# print byteorder of current working architecture
+print(f"Byte order: {sys.byteorder}")
+
+print(f"Size of Python objects: {sys.getsizeof(1)}")
+
+print(f"Platform: {sys.platform}")
+
+# Limiting code to run on specific version
+
+# If less than 3 
+if sys.version_info.major < 3:
+    print("You need to update Python")
+elif sys.version_info.minor <= 7:
+    print("You are not running the latest version of Python")
+else:
+    print("Your Python version is compatible")
