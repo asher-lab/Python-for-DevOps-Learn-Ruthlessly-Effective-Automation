240f218ade8fbc06741f374671f21377fd74e2d6
Asher <51806917+asher-lab@users.noreply.github.com>
2021-04-27 09:49:24 +0800

    Update click.py

--- Chapter_3_Working_with_the_command_line/click.py
+++ Chapter_3_Working_with_the_command_line/click.py
@@ -1,1 +1,16 @@
- 
+import click
+
+@click.command()
+@click.option('--greeting', default='Hiya',help='How do you want to greet?')
+@click.option('--name', default='Tammy',help='Who do you wan to greet?')
+
+#define the greet function
+def greet (greeting,name):
+    print(f"{greeting} {name}")
+    
+if __name__=='__main__':
+    greet()
+  
+  
+#usage
+#./click.py --greeting Hola --name Erik!
