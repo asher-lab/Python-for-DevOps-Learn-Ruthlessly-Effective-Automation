58ad26f703810ba2d21de22636fbf27dddd50192
Asher <51806917+asher-lab@users.noreply.github.com>
2021-04-29 07:44:16 +0800

    Update follow.py

--- Chapter_4_Useful_Linux_Utilities/follow.py
+++ Chapter_4_Useful_Linux_Utilities/follow.py
@@ -5,3 +5,4 @@ subprocess.call(['ls','-alh'])
 # run a subprocess,like in bash ls -alh
 
 #usage = strace python3 follow.py
+# strace is a process of tracing the path of the program starting from its CLI call up until the command is finished
